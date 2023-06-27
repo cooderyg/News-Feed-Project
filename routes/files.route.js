526ee13a89d9681bbe00b507ae81e097fd9892c0
cooderyg <ygk9604@naver.com>
2023-06-27 16:43:06 +0900

    s3 업로드완료

--- routes/files.route.js
+++ routes/files.route.js
@@ -3,8 +3,9 @@ const upload = require("../s3/s3.js");
 const router = express.Router();
 
 router.post("/", upload.single("photo"), async (req, res) => {
-  const photo = req.files;
-  console.log(photo);
+  const imageUrl = req.file.location;
+  console.log(imageUrl);
+  res.status(200).json({ data: imageUrl });
 });
 
 module.exports = router;
--- s3/s3.js
+++ s3/s3.js
@@ -14,9 +14,6 @@ const storage = multer_s3({
   bucket: process.env.BUCKET_NAME, // 자신의 s3 버킷 이름
   contentType: multer_s3.AUTO_CONTENT_TYPE,
   acl: "public-read", // 버킷에서 acl 관련 설정을 풀어줘야 사용할 수 있다.
-  metadata: function (req, file, cb) {
-    cb(null, { fieldName: file.fieldname });
-  },
   key: function (req, file, cb) {
     cb(null, `image/review/${Date.now().toString()}-${file.originalname}`);
   },
