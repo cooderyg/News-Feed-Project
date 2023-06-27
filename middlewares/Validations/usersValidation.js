102f8e2f8639166bc292898abf99454608ef4efc
inhanbyeol <inhanbyeol94@gmail.com>
2023-06-27 15:47:48 +0900

    user Validation add

---
+++ middlewares/Validations/usersValidation.js
@@ -0,0 +1,31 @@
+const Joi = require('joi');
+const { user } = require('./message.json');
+
+const userValidation = {
+  signin: async (req, res, next) => {
+    const body = req.body;
+    const schema = Joi.object().keys({
+      email: Joi.string()
+        .max(30)
+        .regex(/^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i)
+        .required()
+        .messages(user.email),
+      password: Joi.string()
+        .min(8)
+        .max(20)
+        .regex(/^(?=.*[a-zA-z])(?=.*[0-9])(?=.*[$`~!@$!%*#^?&\\(\\)\-_=+])/)
+        .required()
+        .messages(user.password),
+    });
+
+    try {
+      await schema.validateAsync(body);
+    } catch (err) {
+      return res.status(412).json({ message: err.message });
+    }
+
+    next();
+  },
+};
+
+module.exports = userValidation;
