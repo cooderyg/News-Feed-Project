ab88bdfc1fcc1d7b94b56160e41394c4f4f0d4c4
choichanggeun <choichanggeoun@gmail.com>
2023-06-27 11:40:41 +0900

    1.test

--- routes/index.route.js
+++ routes/index.route.js
@@ -1,4 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
+const placesRouter = require("./routes/places.route");
+
+router.use("/places,placesRoute");
 module.exports = router;
---
+++ routes/places.route.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+
+// 맛집 API
+
+router.post("/places", async (req, res) => {});
+
+module.exports = router;
