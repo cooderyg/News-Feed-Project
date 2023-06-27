c82a62001b41726ee5769ea091807e10357a6521
inhanbyeol <inhanbyeol94@gmail.com>
2023-06-27 10:43:10 +0900

    express-session 미들웨어 등록

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
+require('dotenv').config();
+
 const express = require('express');
-const cookieParser = require('cookie-parser');
-const router = require('./routes/index.route.js');
 const session = require('express-session');
 const MemoryStore = require('memorystore')(session);
+const router = require('./routes/index.route.js');
+const { SESSION_SECRET_KEY } = process.env;
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
-app.use(cookieParser());
+app.use(
+  session({
+    secret: SESSION_SECRET_KEY,
+    resave: false,
+    rolling: true,
+    saveUninitialized: false,
+    store: new MemoryStore({ checkPeriod: 1000 * 60 * 60 }),
+    cookie: {
+      maxAge: 1000 * 60 * 60,
+    },
+  })
+);
 
 app.use('/api', router);
 
