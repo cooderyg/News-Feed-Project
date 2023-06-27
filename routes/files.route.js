c40a89e42691e43e890a49c8e6ec88e09a66eab6
inhanbyeol <inhanbyeol94@gmail.com>
2023-06-27 16:13:46 +0900

    sequelize init

--- routes/files.route.js
+++ routes/files.route.js
@@ -2,9 +2,9 @@ const express = require('express');
 // const upload = require("../s3/s3.js");
 const router = express.Router();
 
-router.post('/', upload.single('photo'), async (req, res) => {
-  const photo = req.files;
-  console.log(photo);
-});
+// router.post("/", upload.single("photo"), async (req, res) => {
+//   const photo = req.files;
+//   console.log(photo);
+// });
 
 module.exports = router;
