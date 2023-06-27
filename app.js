edf6cd71e88fb2ba571e832c00b85ee7f978d982
inhanbyeol <inhanbyeol94@gmail.com>
2023-06-27 09:21:46 +0900

    테스트

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const express = require("express");
-const cookieParser = require("cookie-parser");
-const router = require("./routes/index.route.js");
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const router = require('./routes/index.route.js');
 
 const app = express();
 const PORT = 3000;
@@ -8,8 +8,8 @@ const PORT = 3000;
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/api", router);
+app.use('/api', router);
 
 app.listen(PORT, () => {
-  console.log(PORT, "포트 번호로 서버가 실행되었습니다.");
+  console.log(PORT, '포트 번호로 서버가 실행되었습니다.');
 });
