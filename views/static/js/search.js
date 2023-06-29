7b8d09c69d3145cc392eb6f89b721154183e9a68
inhanbyeol <inhanbyeol94@gmail.com>
2023-06-29 23:52:06 +0900

    [추가] 검색 페이지 스켈레톤 구현

--- views/static/js/search.js
+++ views/static/js/search.js
@@ -9,3 +9,16 @@ const search = () => {
 searchBtn.addEventListener('click', search);
 searchBox.addEventListener('keypress', (e) => (e.keyCode == 13 ? search() : null));
 searchBox.focus();
+
+// 스켈레톤 요소
+const skeletonItem = document.querySelectorAll('.skeleton_loading');
+// 스켈레톤 요소 전체 삭제
+const hideskeleton = () => {
+  skeletonItem.forEach((element) => {
+    $(element).fadeOut();
+  });
+};
+// 테스트 코드 (페이지 로딩을 위해 2초간 스켈레톤 애니메이션이 보여짐)
+// window.onload = setTimeout(hideskeleton, 2000);
+// 실제 코드 (실제로 사용될 코드)
+window.onload = hideskeleton;
