94ab70874e2f7fa90dfbc8ab181ac7cd2532b951
inhanbyeol <inhanbyeol94@gmail.com>
2023-06-29 19:42:39 +0900

    modal 작업 중

--- views/static/js/mypage.js
+++ views/static/js/mypage.js
@@ -52,3 +52,24 @@ const getLike = async () => {
 };
 
 getLike();
+
+//modal 영역
+const modal = document.getElementById('modal');
+
+function modalOn() {
+  modal.classList.add('on');
+}
+function modalOff() {
+  modal.classList.remove('on');
+}
+const btnModal = document.querySelector('.profile-write');
+btnModal.addEventListener('click', modalOn);
+
+const closeBtn = document.querySelector('.close-area');
+modal.addEventListener('click', function (e) {
+  if (e.target === this) {
+    modalOff();
+  }
+});
+
+closeBtn.addEventListener('click', modalOff);
